92acea5fdf285b107858d61247491c8424ab8625
zamonia500 <zamonia500@gmail.com>
2017-10-26 17:13:44 +0900

    hwloooooool

---
+++ 수업/171026/171026.py
@@ -0,0 +1,23 @@
+def is_in(collection, data):
+    """
+    
+    :param collection: data를 가지고 있는지 확인할 sequence data type collection
+    :param data: finding data
+    :return: True - collection이 data를 가지고 있으면. False - 없으면
+    """
+
+def reversing(collection):
+    """
+    collection의 순서를 뒤집은 list를 return 해 주는 함수
+    :param collection: 
+    :return: 
+    """
+
+def count(collection, item):
+    """
+    collection이 몇 개의 item을 가지고 있는지 알려주는 함수
+    :param collection: 
+    :param item: 
+    :return: 
+    """
+
